2c90f70364404cc357adba981586256fb6f0d0f5
leehseub <leehseub@naver.com>
2026-03-20 19:16:37 +0900

    [CHORE] 더미 데이터 추가 및 App.tsx에서 TodoCard 렌더링 연결

---
+++ src/mocks/todos.ts
@@ -0,0 +1,7 @@
+import type { Todo } from '../types/todo';
+
+export const DUMMY_TODOS: Todo[] = [
+  { id: '1', title: '할 일 1', completed: false },
+  { id: '2', title: '할 일 2', completed: true },
+  { id: '3', title: '할 일 3', completed: false },
+];
